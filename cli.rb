1caa9e0ee547411c9344e11a0db43cc2c0da2f02
Alexander Bach <alexander.bach@aroundhome.de>
2022-05-20 15:12:30 +0200

    WIP analyze spaghetti carbonara

--- cli.rb
+++ cli.rb
@@ -1,4 +1,22 @@
 require 'net/http'
+require 'uri'
+require 'yaml'
+require 'json'
 
-content = Net::HTTP.get('example.com', '/index.html')
+api_key = YAML.load_file('config').fetch('spoonacular_apikey', nil)
+
+recipe = {"title": "Spaghetti Carbonara",
+          "servings": 2,
+          "ingredients": [
+            "1 lb spaghetti",
+            "3.5 oz pancetta",
+            "2 Tbsps olive oil",
+            "1  egg",
+            "0.5 cup parmesan cheese"
+          ],
+          "instructions": "Bring a large pot of water to a boil and season generously with salt. Add the pasta to the water once boiling and cook until al dente. Reserve 2 cups of cooking water and drain the pasta. "
+          }
+puts api_key
+content = Net::HTTP.post(URI("https://api.spoonacular.com/recipes/analyze?apiKey=#{api_key}"), recipe.to_json, "Content-Type" => "application/json")
 puts(content)
+puts(content.body)
