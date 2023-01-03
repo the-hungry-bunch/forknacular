cfcbb08436177dfdf6cf1dc67aca57cb388b5004
Tobias Bach <tobias.bach@aroundhome.de>
2023-01-03 17:10:58 +0100

    Clean up the repo to build a fundament for following tasks

--- forknacular.rb
+++ forknacular.rb
@@ -1,24 +1,7 @@
 require 'sinatra'
 require './cli'
 
-
-get '/' do
-  'Was ganz anderes'
-end
-
-get '/woanders' do
-  'Hallo von woanders'
-end
-
-get '/recipe' do
-  input = params["text"]
-  #output = api_call(input)
-  output = foodwarning 
-  erb :index, :locals => {:wineText => output}
-end
-
 get '/warning' do
-  # input
-  output = foodwarning
+  output = foodwarning(params['amount'])
   erb :index, :locals => {:warningTextFromAPI => output}
 end
