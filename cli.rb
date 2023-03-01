e616ecef48ecfde405b3de79a545aaf982b3a546
Chris <christoph.schulz@aroundhome.de>
2023-03-01 11:47:48 +0100

    added links to output/README

--- cli.rb
+++ cli.rb
@@ -16,7 +16,7 @@ def foodwarning(amount)
         responseJSON = JSON.parse(response.body)
         #require 'pry'; binding.pry
         responseJSON.dig('response', 'docs').each do |warning|
-            hash[warning['title']] = [warning['warning'], warning['product']['imageUrls'].first]
+            hash[warning['title']] = [warning['warning'], warning['product']['imageUrls'].first, warning['link']]
         end
     end
     hash
