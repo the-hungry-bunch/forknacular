8d815d017a164dc55c6527b4613f73b0d35e9a7e
Chris <christoph.schulz@aroundhome.de>
2023-06-01 16:48:01 +0200

    bugs & wording fixed

--- tictactoe.rb
+++ tictactoe.rb
@@ -23,30 +23,29 @@ end
 
 def playerRound(arr) 
     free = true
-    row = -1
-    column = -1
+    row, column = -1, -1
     while free
         while row <= 0 || row > 3 do
             puts "TicTacToe Game: Bitte horizontale Koordinate eintragen (1 bis 3)"
             row = gets.chomp.to_i
         end
 
-        puts "TicTacToe Game: Bitte verticale Koordinate eintragen (1 bis 3)"
-        column = gets.chomp.to_i
-
         while column <= 0 || column > 3 do
-            puts "Bitte eine Zahl von 1-3"
+            puts "TicTacToe Game: Bitte verticale Koordinate eintragen (1 bis 3)"
             column = gets.chomp.to_i
         end
 
         free = free?(arr, row, column)
         if free == false
             puts "Feld schon belegt, bitte neue Eingabe starten"
+            free = true
+            row, column = -1, -1
         else
             arr[row-1][column-1] = 'O'
             free = false
         end
     end
+    puts arr
 end
 
 def computerRound(arr)
@@ -69,8 +68,8 @@ def check_winner(arr)
     end
     
     #diagonals check
-    if (arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2]) || 
-       (arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0])
+    if (arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2] && arr[0][0] != nil) || 
+       (arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0] && arr[0][2] != nil)
        gameover = true
     end
 
@@ -89,7 +88,7 @@ while true
     computerRound(arr)
     if check_winner(arr)
         field(arr)
-        puts "Yay! Du (PC) hast gewonnen!"
+        puts "Nooo! PC hat gewonnen!"
         break
     end
 end
