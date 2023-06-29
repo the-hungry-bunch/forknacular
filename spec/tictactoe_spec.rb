2f4d0faa22f3e1d31012d7106b41e6aa214da7e9
Chris <christoph.schulz@aroundhome.de>
2023-06-29 17:04:49 +0200

    testing computerRound

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -1,21 +1,37 @@
 require_relative '../tictactoe'
 
 RSpec.describe TicTacToe do
-    context "start with empty fields" do
-        let(:row) { 1 }
-        let(:column) { 1 }
-        let(:field) { [[nil, nil, nil], [nil ,nil ,nil ], [nil, nil, nil]] }
-        it "detects empty fields" do
-            actual = subject.free?(field, row, column)
-            expect(actual).to eq true
-        end
-        context "second turn" do
-            let(:field) { [['X', nil, nil], [nil ,nil ,nil ], [nil, nil, nil]] }
-            it "detects non-empty fields" do
+    describe "#free?" do
+        context "start with empty fields" do
+            let(:row) { 1 }
+            let(:column) { 1 }
+            let(:field) { [[nil, nil, nil], [nil ,nil ,nil ], [nil, nil, nil]] }
+            it "detects empty fields" do
                 actual = subject.free?(field, row, column)
-                expect(actual).to eq false
+                expect(actual).to eq true
+            end
+            context "second turn" do
+                let(:field) { [['X', nil, nil], [nil ,nil ,nil], [nil, nil, nil]] }
+                it "detects non-empty fields" do
+                    actual = subject.free?(field, row, column)
+                    expect(actual).to eq false
+                end
             end
         end
     end
-
+    describe "computerRound" do
+        context "computer first turn" do
+            let(:field) { [['O', nil, nil], [nil ,nil ,nil ], [nil, nil, nil]] }
+            it "chooses an empty field" do
+                subject.computerRound(field)
+                expect(field[0][1]).to eq 'X'
+            end
+        end
+        context "computer last turn, field full" do
+            let(:field) { [['O', 'X', 'X'], ['X', 'X', 'O'], ['O', 'X', 'O']] }
+            it "tells us the game has ended, no valid plays available" do
+                expect { subject.computerRound(field) }.to raise_error(NoMethodError,"undefined method `divmod' for nil:NilClass")
+            end
+        end
+    end
 end
