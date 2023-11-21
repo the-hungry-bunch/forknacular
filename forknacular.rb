7d7fa79f4e927e5e1b01f7152c599a8ae62c57da
Chris <christoph.schulz@aroundhome.de>
2023-11-21 17:35:29 +0100

    overview created

--- forknacular.rb
+++ forknacular.rb
@@ -3,5 +3,13 @@ require './cli'
 
 get '/warning' do
   output = foodwarning(params['amount'])
-  erb :index, :locals => {:warningTextFromAPI => output}
+  erb :index, locals: {warningTextFromAPI: output}
+end
+
+get '/' do
+  erb :overview
+end
+
+get '/tictactoe' do
+  "Tictactoe"
 end
