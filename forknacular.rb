04aa43b9b72033e0d3a93f65571973d19ae66481
Chris <christoph.schulz@aroundhome.de>
2024-01-09 17:13:01 +0100

    readme updated, new post end point added

--- forknacular.rb
+++ forknacular.rb
@@ -14,3 +14,9 @@ get '/tictactoe' do
   output = params['field'] || [["a","b","c"],["1",nil,"3"],[nil,"5",nil]]
   erb :tictactoe, locals: {field: output}
 end
+
+post '/tictactoe' do
+  require 'pry';  binding.pry
+  output = params['field'] || [["b","b","c"],["1",nil,"3"],[nil,"5",nil]]
+  erb :tictactoe, locals: {field: output}
+end
