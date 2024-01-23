2bada8c3ec131712a43503f0fdee4ebc0a2ec3ea
Anna Varamezova <anna.varamezova@gmail.com>
2024-01-23 17:05:13 +0100

    creating a before and after hash to be used later for comparing last values and input

--- forknacular.rb
+++ forknacular.rb
@@ -17,6 +17,17 @@ end
 
 post '/tictactoe' do
   require 'pry';  binding.pry
+  before = hash.new
+  after = hash.new
+  params.each_key do |key|
+    if key.start_with?('before_')
+      before[key] = params[key]
+    else
+      after[key] = params[key]
+    end
+  end
   output = params['field'] || [["b","b","c"],["1",nil,"3"],[nil,"5",nil]]
   erb :tictactoe, locals: {field: output}
 end
+
+
