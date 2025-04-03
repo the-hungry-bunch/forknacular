11751e81c992c2024329791b9645fca712e4f5fd
ChrisB <Christoph.Schulz_@gmx.net>
2025-04-03 17:08:52 +0200

    added overview & tictactoe pages
    Need for overview to discover/navigate to pages
    tictactoe page does not do anything for now

--- forknacular.rb
+++ forknacular.rb
@@ -5,3 +5,12 @@ get '/warning' do
   output = foodwarning(params['amount'])
   erb :index, :locals => {:warningTextFromAPI => output}
 end
+
+get '/' do
+  erb :overview
+end
+
+get '/tictactoe' do
+  output = params['field'] || [[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]]
+  erb :tictactoe, locals: {field: output}
+end
